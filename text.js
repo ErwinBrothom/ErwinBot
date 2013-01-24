834eaa63be12807eba3268a829790bea7090e935
Erwin Haasnoot <erwinhaasnoot@gmail.com>
2013-01-24 01:23:08 +0100

    5

--- text.js
+++ text.js
@@ -5,11 +5,24 @@
 
 var text = {
     getLine : function() {
-        return this.lines[1].getText();
+        return this.getRandomLine().getText();
     },
     lines : [
         Line("Dit is tekst"),
         Line("Dit is ook tekst"),
-    ]
+    ],
+    getRandomLine : function() {
+        counter = 0, foundLine = null;
+        while ( counter < 5 && foundLine === null) {
+            randno = Math.floor ( Math.random() * this.lines.length);
+            if(this.lines[randno].filterFunc()) {
+                foundLine = this.lines[randno];
+            }
+        }
+        if (foundLine == null) {
+            foundLine = Line("Sorry, ik sta met mijn mond vol tanden");
+        }
+        return foundLine;
+    }
 }
 
